902b5071e49a6559f01b09c626acae8042f28245
robotastic <luke@robotastic.com>
2016-04-25 22:43:45 -0400

    whoops

--- examples/peripheral-explorer.js
+++ examples/peripheral-explorer.js
@@ -167,7 +167,7 @@ function explore(peripheral) {
                         });
                     },
                     function (err) {
-
+                        peripheral.disconnect();
                     }
                 );
             });
